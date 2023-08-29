6fb53b0fdc101784c38c60b5ead0d162f1a52cbc
Rinseo <jack_11728@msn.com>
2023-08-29 14:17:22 +0200

    session2

---
+++ lesson2/lesson2/lesson2App.swift
@@ -0,0 +1,17 @@
+//
+//  lesson2App.swift
+//  lesson2
+//
+//  Created by Jack Xia on 29/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct lesson2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
