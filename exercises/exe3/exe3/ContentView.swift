aef2c16cf9f7e12b7cd2e0cdb7d8e1f81809a821
Rinseo <jack_11728@msn.com>
2023-09-12 15:32:24 +0200

    Oppgave 6 - Added btn to open sheet that all allows input and appending to an array

--- exercises/exe3/exe3/ContentView.swift
+++ exercises/exe3/exe3/ContentView.swift
@@ -9,18 +9,76 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let contacts = [
+    @State var contacts = [
         Contact.init(firstName: "John", lastName: "Wick", description: "A very dangerous man", img: "coder"),
         Contact.init(firstName: "Motoko", lastName: "Kusanagi", description: "", img: "linux"),
         Contact.init(firstName: "Batou", lastName: "", description: "", img: "kodama"),
     ]
     
+    // Button to add contact
+    @State var newContactFName: String = ""
+    @State var newContactLName: String = ""
+    
+    // Sheets is showing if true
+    @State var isPresentingAddContactView: Bool
+    // But at load set to false
+    init(isPresentingAddContactView: Bool = false) {
+        self.isPresentingAddContactView = isPresentingAddContactView
+    }
+    
+    func addContact() {
+        let newContact = Contact(firstName: newContactFName, lastName: newContactLName)
+        contacts.append(newContact)
+        print(contacts)
+    }
     
     var body: some View {
         NavigationStack {
-            
             // Refactored this List into a separated file.
              ContactVerticalListView(contacts: contacts)
+            
+            
+            VStack {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 15)
+                        .frame(width: 120, height: 50)
+                    Text("Add contact")
+                        .foregroundColor(.white)
+                }.onTapGesture {
+                    isPresentingAddContactView = true
+                    print("Sheet showing")
+                }
+                .padding(EdgeInsets(top: 15, leading: 0, bottom: 0, trailing: 0))
+            } // add new Contact button
+            .sheet(isPresented: $isPresentingAddContactView) {
+                
+                // Sheet content View
+                VStack {
+                    VStack {
+                        
+                        TextField("First name", text: $newContactFName)
+                            .frame(width: 150)
+                        TextField("Last name", text: $newContactLName)
+                            .frame(width: 150)
+                        
+                    }
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 15)
+                            .frame(width: 120, height: 50)
+                        Text("Save")
+                            .foregroundColor(.white)
+                    }.onTapGesture {
+                        addContact()
+                        print("Saving..")
+                        isPresentingAddContactView = false
+                    }
+                }
+            } // Sheet
+            
+            
+            
+            
+            
             
             
             // TODO: Need to refactor this into a horizontal View
@@ -37,9 +95,9 @@ struct ContentView: View {
 //                }
 //            }
             
-            
         } // nav stack
-    }
+        
+    } // body
 }
 
 
