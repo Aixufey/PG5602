c5f245efc154abc044ad97b4cd29a0d57be7281f
Rinseo <jack_11728@msn.com>
2023-09-12 13:59:17 +0200

    Lesson 6 continue with closures, refactored UpdateProduct into a new View

---
+++ lesson3/lesson3/UpdateProductView.swift
@@ -0,0 +1,20 @@
+//
+//  UpdateProductView.swift
+//  lesson3
+//
+//  Created by Jack Xia on 12/09/2023.
+//
+
+import SwiftUI
+
+struct UpdateProductView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UpdateProductView_Previews: PreviewProvider {
+    static var previews: some View {
+        UpdateProductView()
+    }
+}
